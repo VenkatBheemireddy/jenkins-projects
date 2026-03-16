a5fb8c349feb081d4c605dfc5b543a9ab8b0dc5f
Ubuntu <ubuntu@ip-172-31-17-226.us-east-2.compute.internal>
2026-03-16 10:37:32 +0000

    changes

--- hello-world/java-app/src/main/java/com/example/HelloWorld.java
+++ hello-world/java-app/src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World from Maven Java Application!");
+        System.out.println("Hello from Maven Application!");
     }
 }
